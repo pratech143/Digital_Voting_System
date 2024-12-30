238e53777a9838653b558242ae91739d840de0ce
Prabhat Acharya <void@WHATTTT.local>
2024-12-30 17:45:57 +0545

    2nd try

--- backend/config/mail_config.php
+++ backend/config/mail_config.php
@@ -1,9 +1,9 @@
 <?php
 function send_otp_email($recipientEmail, $otp) {
     $subject = "Your Login OTP";
-    $message = "Dear User,\n\nYour One-Time Password is: $otp\n\nPlease use this OTP within 5 minutes.\n\nThank you,\ne-मत Team";
+    $message = "Dear User,\n\nYour One-Time Password is: $otp\n\nPlease use this OTP within 5 minutes.\n\nThank you,\nDeveloper Team";
     $headers = "From: noreply@e-mat.com\r\n";
-    $headers .= "Reply-To: noreply@e-mat.com\r\n";
+    $headers .= "Reply-To: noreply@e-election.com\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
     if (mail($recipientEmail, $subject, $message, $headers)) {
